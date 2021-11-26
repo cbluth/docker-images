bed3d6754d8c1d65c83873e1a9dce3d6b101511c
Simone Silenzi <44733864+ssilenzi@users.noreply.github.com>
2021-11-26 02:33:09 +0100

    Update noetic-light.sh

--- plwitico/noetic-light.sh
+++ plwitico/noetic-light.sh
@@ -151,7 +151,7 @@ ${DOCKER_EXECUTABLE:-docker} run \
     --env="XAUTHORITY=$XAUTH" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="$XAUTH:$XAUTH" \
-    --volume=$(dirname $0)/gurobi.lic:/opt/gurobi950/gurobi.lic:ro \
+    --volume=$(dirname "$0")/gurobi.lic:/opt/gurobi950/gurobi.lic:ro \
     ${NAME_OPTION:-} \
     ${DOCKER_PARAMS:-} \
     $@ ${DUMMY_DEFAULTS:-}
