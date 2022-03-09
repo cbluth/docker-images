c047b4e27fbc8ab4245f2c5d3ebb19c6efb7cffc
Simone Silenzi <44733864+ssilenzi@users.noreply.github.com>
2022-03-09 12:07:10 +0100

    Update melodic-init.sh

--- plwitico/melodic-init.sh
+++ plwitico/melodic-init.sh
@@ -3,7 +3,7 @@ git config --global user.email 'your@email.com' && \
 git config --global user.name 'Your Name' && \
 git config --global url.https://aaaaaaaa@github.com/.insteadOf https://github.com/ && \
 git clone --branch ${ROS_DISTRO}-devel --recurse-submodules https://github.com/CentroEPiaggio/planning-with-tight-constraints.git . && \
-catkin config --extend /opt/ros/$ROS_DISTRO --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && \
+catkin config --extend /opt/ros/$ROS_DISTRO --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && \
 ( \
     echo ''; \
     echo '# source project'; \
